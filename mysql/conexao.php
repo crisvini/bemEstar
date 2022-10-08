cecdd8ed461472c224ad469a64331d0ab451be45
Cristian <crisvini.leoncini@outlook.com>
2022-10-07 22:01:51 -0300

    first commit

---
+++ mysql/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$user = "workou68_admin";
+$password = "15C25v35l45l$";
+$database = "workou68_web";
+
+$mysqli = new mysqli($server, $user, $password, $database);
+if ($mysqli->connect_errno)
+    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " .  $mysqli->connect_error;
